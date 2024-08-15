c3a4ee222cdfd4bd5855e84afec73b927719dad4
DavidI <kamsidavid2@gmail.com>
2024-08-15 09:57:16 +0100

    query a subreddit for subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
 import requests
+"""This script queries the Reddit and returns a given subreddit's
+    subscriber number"""
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    '''
+    Queries the Reddit API and return the number of subscribers
+        (not active users, total subscribers) for a given subreddit
+
+    Args:
+        subreddit(str): The subreddit
+
+    Returns:
+        int: number of subscribers for the given subreddit
+                0 if subreddit is invalid
+    '''
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': '0-subs:1.0 by daverazon'}
+    # Add user agent to prevent rate limiting
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    elif response.status_code == 404:
+        # 404 is status code for not found
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
